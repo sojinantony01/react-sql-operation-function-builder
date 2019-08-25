7467f5f867719fa93925d3281c1eb9d4935e4722
Sojin <sojin.antony01@gmail.com>
2019-08-25 23:58:06 +0530

    Updates

---
+++ precache-manifest.ed17743b551e7eb5490f0901b097b2a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e68b9fdca40bdf85d00c083013714b32",
+    "url": "/react-sql-operation-function-builder/index.html"
+  },
+  {
+    "revision": "1592aedf6af73a766d2e",
+    "url": "/react-sql-operation-function-builder/static/css/2.e8d046a2.chunk.css"
+  },
+  {
+    "revision": "b1c2488e6baf426d7ccc",
+    "url": "/react-sql-operation-function-builder/static/css/main.690e02d4.chunk.css"
+  },
+  {
+    "revision": "1592aedf6af73a766d2e",
+    "url": "/react-sql-operation-function-builder/static/js/2.cefb8ae4.chunk.js"
+  },
+  {
+    "revision": "b1c2488e6baf426d7ccc",
+    "url": "/react-sql-operation-function-builder/static/js/main.f068bfd7.chunk.js"
+  },
+  {
+    "revision": "90af063c94431eaf3602",
+    "url": "/react-sql-operation-function-builder/static/js/runtime~main.20b5db33.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7a0b609f3afba1828e810c45d526a197.js"
+  "/react-sql-operation-function-builder/precache-manifest.ed17743b551e7eb5490f0901b097b2a9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-sql-operation-function-builder/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
